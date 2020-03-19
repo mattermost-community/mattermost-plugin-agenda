546695b1976d6ddcad3f50c15e98ad84c87250f2
marianunez <maria.nunez@mattermost.com>
2020-03-19 10:03:22 -0400

    Linting

--- webapp/src/components/meeting_settings/meeting_settings.jsx
+++ webapp/src/components/meeting_settings/meeting_settings.jsx
@@ -117,13 +117,13 @@ export default class MeetingSettingsModal extends React.PureComponent {
                             className='form-control'
                             value={this.state.hashtag ? this.state.hashtag : ''}
                         />
-                        <p className='text-muted pt-1'> {'Hashtag is formatted using the '}	
-                            <a	
-                                target='_blank'	
-                                rel='noopener noreferrer'	
-                                href='https://godoc.org/time#pkg-constants'	
-                            >{'Go time package.'}</a>	
-                            {' Embed a date by writing down what January 2, 2006 would look like.'}	
+                        <p className='text-muted pt-1'> {'Hashtag is formatted using the '}
+                            <a
+                                target='_blank'
+                                rel='noopener noreferrer'
+                                href='https://godoc.org/time#pkg-constants'
+                            >{'Go time package.'}</a>
+                            {' Embed a date by writing down what January 2, 2006 would look like.'}
                         </p>
                     </div>
                 </Modal.Body>
