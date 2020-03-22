b767075889d8ae535fc685b17858695dc213e232
Maria A Nunez <maria.nunez@mattermost.com>
2020-03-22 16:39:01 -0400

    GH-18 - Improve hashtag help text (#19)
    * Improved date format help text
    
    * Updated help text
    
    * Linting

--- webapp/src/components/meeting_settings/meeting_settings.jsx
+++ webapp/src/components/meeting_settings/meeting_settings.jsx
@@ -117,12 +117,13 @@ export default class MeetingSettingsModal extends React.PureComponent {
                             className='form-control'
                             value={this.state.hashtag ? this.state.hashtag : ''}
                         />
-                        <p className='text-muted pt-1'> {'Date format should follow the one of  '}
+                        <p className='text-muted pt-1'> {'Hashtag is formatted using the '}
                             <a
                                 target='_blank'
                                 rel='noopener noreferrer'
-                                href='https://yourbasic.org/golang/format-parse-string-time-date-example/#standard-time-and-date-formats'
-                            >{'the predefined layouts'}</a>
+                                href='https://godoc.org/time#pkg-constants'
+                            >{'Go time package.'}</a>
+                            {' Embed a date by writing down what January 2, 2006 would look like.'}
                         </p>
                     </div>
                 </Modal.Body>
