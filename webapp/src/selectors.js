8b07bbf2f65bc3025f69d95ca919ec65e9613e3f
Jesse Hallam <jesse.hallam@gmail.com>
2020-03-24 16:01:33 -0300

    initialize default redux state from reducer
    The selectors assume a fully populated initial state, and currently result in resolving a property on an undefined object. By falling back to the default state from the reducer (before Redux itself kicks in), we avoid this race condition and allow normal usage of the selectors.

--- webapp/src/selectors.js
+++ webapp/src/selectors.js
@@ -1,6 +1,8 @@
 import {id as pluginId} from './manifest';
+import reducer from './reducer';
 
-const getPluginState = (state) => state['plugins-' + pluginId] || {};
+const defaultState = reducer({}, {});
+const getPluginState = (state) => state['plugins-' + pluginId] || defaultState;
 
 export const getMeetingSettingsModalState = (state) => getPluginState(state).meetingSettingsModal;
 export const getMeetingSettings = (state) => getPluginState(state).meetingSettings;
