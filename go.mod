21a08366b4f371ad38e184857cff940586fa767f
Maria A Nunez <maria.nunez@mattermost.com>
2020-06-25 17:32:19 -0400

    GH-32- Fix issue with agenda list numbering (#44)
    Co-authored-by: Ben Schumacher <ben.schumacher@mattermost.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/mattermost/mattermost-plugin-agenda
 go 1.12
 
 require (
-	github.com/blang/semver v3.6.1+incompatible // indirect
-	github.com/mattermost/mattermost-server/v5 v5.24.0
+	github.com/mattermost/mattermost-server/v5 v5.3.2-0.20200618173917-f513fa103726
 	github.com/mholt/archiver/v3 v3.3.0
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.5.1
--- server/command.go
+++ server/command.go
@@ -138,16 +138,19 @@ func (p *Plugin) executeCommandQueue(args *model.CommandArgs) *model.CommandResp
 		return responsef("Error calculating hashtags")
 	}
 
-	itemsQueued, appErr := p.API.SearchPostsInTeam(args.TeamId, []*model.SearchParams{{Terms: hashtag, IsHashtag: true}})
+	searchResults, appErr := p.API.SearchPostsInTeamForUser(args.TeamId, args.UserId, model.SearchParameter{Terms: &hashtag})
 
 	if appErr != nil {
-		return responsef("Error getting user")
+		return responsef("Error calculating list number")
 	}
+
+	postList := *searchResults.PostList
+	numQueueItems := len(postList.Posts)
 
 	_, appErr = p.API.CreatePost(&model.Post{
 		UserId:    args.UserId,
 		ChannelId: args.ChannelId,
-		Message:   fmt.Sprintf("#### %v %v) %v", hashtag, len(itemsQueued)+1, message),
+		Message:   fmt.Sprintf("#### %v %v) %v", hashtag, numQueueItems+1, message),
 	})
 	if appErr != nil {
 		return responsef("Error creating post: %s", appErr.Message)
