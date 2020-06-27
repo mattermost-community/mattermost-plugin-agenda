1218e16e18c46f347f512fe99782a6012ec91c83
Fedor Vitkovskiy <fedorvitkovskiy@pop-os.localdomain>
2020-06-27 09:38:36 +0200

    Remove getCommonHeaders, make style changes

--- webapp/src/client.js
+++ webapp/src/client.js
@@ -16,35 +16,34 @@ export default class Client {
     }
 
     doGet = async (url, headers = {}) => {
-        return this.doFetch(url, { headers });
+        return this.doFetch(url, {headers});
     }
 
     doPost = async (url, body, headers = {}) => {
         return this.doFetch(url, {
             method : 'POST',
             body: JSON.stringify(body),
-            headers: {...headers, ...{
-                'Content-Type' : 'application/json'
-            }}
+            headers: {
+                ...headers, 
+                'Content-Type' : 'application/json',
+            }
         });
     }
 
     doFetch = async (url, { method = 'GET', body = null, headers = {} }) => {
-        const response = await fetch(url, {
+        const options = Client4.getOptions({
             method,
             body,
-            headers: {...this.getCommonHeaders(), ...headers}
+            headers: {
+                ...headers,
+                'X-Timezoone-Offset' : new Date().getTimezoneOffset(),
+                'Accept' : 'application/json'
+            },
         });
+
+        const response = await fetch(url, options);
 
         return response.json();
     }
 
-    getCommonHeaders = () => {
-        const { headers } = Client4.getOptions([]);
-
-        headers['X-Timezone-Offset'] = new Date().getTimezoneOffset();
-        headers['Accept'] = 'application/json';
-
-        return headers;
-    }
 }
