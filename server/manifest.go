994c206a1f654f0caed25aaad5a645e84b2490b0
Ben Schumacher <ben.schumacher@mattermost.com>
2020-11-20 19:08:55 +0100

    Release v0.2.2 (#73)

--- server/manifest.go
+++ server/manifest.go
@@ -17,8 +17,8 @@ const manifestStr = `
   "description": "Plugin to handle meeting agendas for Mattermost channels.",
   "homepage_url": "https://github.com/mattermost/mattermost-plugin-agenda",
   "support_url": "https://github.com/mattermost/mattermost-plugin-agenda/issues",
-  "release_notes_url": "https://github.com/mattermost/mattermost-plugin-agenda/releases/tag/v0.2.1",
-  "version": "0.2.1",
+  "release_notes_url": "https://github.com/mattermost/mattermost-plugin-agenda/releases/tag/v0.2.2",
+  "version": "0.2.2",
   "min_server_version": "5.26.0",
   "server": {
     "executables": {
